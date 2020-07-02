809ba15b5461c2fcd97bdecb11cc845d60476406
Sanyam Bhaskar <sanyambhaskar5@gmail.com>
2020-07-03 05:00:09 +0530

    Fixed a major bug
    Added Tv functionalities
    Next:-
    Sugestions based on Genre
    Favourite Paradigm
    Episode Navigation Feature

--- Script/searchjs/result.js
+++ Script/searchjs/result.js
@@ -1,12 +1,15 @@
 var req = new XMLHttpRequest();
-req.open('GET', 'https://api.themoviedb.org/3/'+type+'/'+query+'?api_key=7ce6f3444cda42a6506370e782b2e857&language=en-US', true);
+req.open('GET', 'https://api.themoviedb.org/3/'+type+'/'+query+'?api_key=7ce6f3444cda42a6506370e782b2e857&language=en-US&append_to_response=external_ids, images', true);
 req.onload = function(){
     var tmdb = JSON.parse(this.responseText);
+    var x;
+    if(type=='tv'){x=tmdb.external_ids.imdb_id;}
+    else{x = tmdb.imdb_id;}
     var subreq = new XMLHttpRequest();
-    subreq.open('GET', 'https://omdbapi.com/?i='+tmdb.imdb_id+'&plot=full&apikey=5ee63658');
+    subreq.open('GET', 'https://omdbapi.com/?i='+x+'&plot=full&apikey=5ee63658');
     subreq.onload = function (){
         var omdb = JSON.parse(this.responseText);
-         //console.log(tmdb, omdb);
+        console.log(tmdb, omdb);
        $('.poster').html('<img src="'+omdb.Poster+'" alt=""><div class="card-footer text-center"><button class="btn btn-primary" style="box-shadow: 1px 1px 5px black;">Add to Favourites <i class="fas fa-heart"></i></button></div>');
         $('#plot').text(tmdb.overview);
         $('#year').text(omdb.Year);
@@ -17,7 +20,7 @@ req.onload = function(){
         
         
         $('#tag').text(tmdb.tagline);
-        $('#title').text(tmdb.original_title);
+        $('#title').text(omdb.Title);
         $('#r1').text(omdb.imdbRating);
         $('#r2').text(omdb.Rated);
         $('#r3').text(omdb.Ratings[omdb.Ratings.length-1].Value);
